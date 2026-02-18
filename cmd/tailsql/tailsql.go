c08e667450d60e07d1e782f73a7985242b8a1e5b
M. J. Fromberger <fromberger@tailscale.com>
2026-02-18 14:10:09 -0800

    all: remove AUTHORS file references in comments
    It turns out I forgot to add the AUTHORS file anyway, and it is not needed.
    
    See tailscale/tailscale#18503 for context

--- cmd/tailsql/tailsql.go
+++ cmd/tailsql/tailsql.go
@@ -1,4 +1,4 @@
-// Copyright (c) Tailscale Inc & AUTHORS
+// Copyright (c) Tailscale Inc & contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
 // Program tailsql runs a standalone SQL playground.
